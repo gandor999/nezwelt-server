cec320473c4bf0849bb18e11404eb4e925c1a64c
Geodor Ruales <geodorruales@gmail.com>
2023-05-08 21:44:01 +0800

    feat: add jwt token generation and authentication

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
 const fs = require("fs");
 
 const { users, territories } = JSON.parse(fs.readFileSync("db.json"));
@@ -22,6 +23,22 @@ const findUser = (res, { reqUsername, reqPassword, users }) => {
     return { username: foundUser.username, roles: foundUser.roles };
 };
 
+const generateAccessToken = (userAuth) => {
+  return jwt.sign(userAuth, "mockSecret", {
+    expiresIn: "1h",
+  });
+};
 
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) res.sendStatus(401);
+
+  jwt.verify(token, "mockSecret", (err, userAuth) => {
+    if (err) res.sendStatus(403);
+    req.user = userAuth;
+    next();
+  });
+}
 
 app.listen(3000);
